9c7c522e164bc47aadeba8b6213dd0023d103cfc
unknown <priyajeetsaxena@gmail.com>
2024-02-08 17:10:25 +0530

    inital commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// index.mjs
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, Express with ESM!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
