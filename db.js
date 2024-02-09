ca6de31588b476a366fa089631c6a5efadaed68d
unknown <priyajeetsaxena@gmail.com>
2024-02-09 13:06:11 +0530

    added search by id request

---
+++ db.js
@@ -0,0 +1,14 @@
+// db.js
+import pg from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+
+const { Pool } = pg;
+
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL,
+});
+
+export default pool;
--- index.js
+++ index.js
@@ -1,30 +1,48 @@
 // index.js
 import express from 'express';
-import pg from 'pg';
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-const { Pool } = pg;
+import  pool from './db.js';
 
 const app = express();
 const port = 3000;
 
-const pool = new Pool({
-  connectionString: process.env.POSTGRES_URL,
-});
+
+// handle favicon request 
+app.get('/favicon.ico', (req, res) => res.status(204));
 
 app.get('/', async (req, res) => {
   try {
-    const response = await pool.query('SELECT book_id, name, price FROM books');
-    return res.status(200).json(response.rows);
+    const { rows } = await pool.query('SELECT book_id, name, price FROM books');
+    return res.status(200).json({ books: rows });
   } catch (error) {
     return res.status(500).json({ error: error.message });
   }
 });
 
+
+app.get("/:id", async(req, res)=>{
+    try {
+        const {rows} = await pool.query('select * from books where book_id = $1',[req.params.id]);
+        if (rows[0]) {
+            return res.json({msg: "OK", data: rows})
+        }
+    } catch (err) {
+     console.log(err)   
+    }
+    console.log(req.params.id)
+    res.send("working")
+})
+
+
+
+// app.post('/', (req, res)=>{
+//     try {
+        
+//     } catch (err) {
+//         long
+//     }
+// })
+
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
-
 
